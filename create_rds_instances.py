b44f6124174c5f4cad428791bacc1b4ac84e7f31
Corby Shaner <corbyshaner@gmail.com>
2017-01-09 15:51:55 -0500

    Added code to determine the ID of the most recent snapshot

--- create_rds_instances.py
+++ create_rds_instances.py
@@ -16,11 +16,12 @@ dir_name = os.environ['S3_DIR_NAME']
 rds_metadata_obj_name = dir_name + '/' + os.environ['RDS_METADATA_FILENAME']
 app_lifecycle_tag = os.environ['APP_LIFECYCLE_TAG']
 week_to_execute = os.environ['WEEK_TO_EXECUTE']
+all_our_snapshots = []
 s3_bucket = s3_resource.Bucket(bucket_name)
 now = datetime.datetime.now()
 
-def create_rds_instances(event, context):
-#def create_rds_instances():
+#def create_rds_instances(event, context):
+def create_rds_instances():
 
     if not check_week_number_of_month(now, week_to_execute):
         return
@@ -53,7 +54,23 @@ def create_rds_instances(event, context):
         # The delete process should keep the most recent RDS-created snapshot.
         for snapshot in snapshots['DBSnapshots']:
             if snapshot_id_partial in snapshot['DBSnapshotIdentifier']:
+                all_our_snapshots.append(snapshot)
+
                 db_snapshot_identifier = snapshot['DBSnapshotIdentifier']
+
+        snapshot_dates = []
+        for snapshot in all_our_snapshots:
+            snapshot_dates.append(snapshot['SnapshotCreateTime'])
+
+        print snapshot_dates
+        snapshot_dates.sort()
+        print snapshot_dates[-1]
+        most_recent_snapshot_date = snapshot_dates[-1]
+
+        for snapshot in all_our_snapshots:
+            if snapshot['SnapshotCreateTime'] == most_recent_snapshot_date:
+                db_snapshot_identifier = snapshot['DBSnapshotIdentifier']
+        print "Latest snapshot date: %s, snapshot id: %s" % (most_recent_snapshot_date, db_snapshot_identifier)
 
         if not db_snapshot_identifier:
             print "DB snapshot partial name not found in list of snapshots: %s" % snapshot_id_partial
@@ -81,16 +98,21 @@ def create_rds_instances(event, context):
         # Create the new instance using the DB snapshot if it exists.
         response = False
         try:
-            response = rds.restore_db_instance_from_db_snapshot(**restore_kwargs)
+            #response = rds.restore_db_instance_from_db_snapshot(**restore_kwargs)
             print "NOTICE: Created DB Instance: %s from DB snapshot: %s" % (db_instance_name, db_snapshot_identifier)
         except Exception, err:
             print "ERROR: Error creating DB Instance %s: %s" % (db_instance_name, err)
 
 def check_week_number_of_month(date_to_check, week_number):
-    if week_number == 'all':
+    if str(week_number).lower() == 'all':
         return True
+    elif str(week_number).lower() == 'first':
+        week_index = 0
+    elif str(week_number).lower() == 'last':
+        week_index = -1
+    else:
+        week_index = int(week_number) - 1
 
-    number_of_full_weeks = 0
     weeks_list = []
     cal = calendar.Calendar()
     weeks_of_month = cal.monthdayscalendar(date_to_check.year, date_to_check.month)
@@ -98,12 +120,11 @@ def check_week_number_of_month(date_to_check, week_number):
         if 0 in week:
             continue
         else:
-            number_of_full_weeks += 1
             weeks_list.append(week)
 
-    if date_to_check.day in weeks_list[week_number - 1]:
+    if date_to_check.day in weeks_list[week_index]:
         return True
     else:
         return False
 
-#create_rds_instances()
+create_rds_instances()
