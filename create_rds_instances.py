0378e8600ba46dd08184864db0534e32805b006c
Corby Shaner <corbyshaner@gmail.com>
2017-01-10 07:58:10 -0500

    Added logic to determine which snapshot is the most recent.

--- create_rds_instances.py
+++ create_rds_instances.py
@@ -16,7 +16,6 @@ dir_name = os.environ['S3_DIR_NAME']
 rds_metadata_obj_name = dir_name + '/' + os.environ['RDS_METADATA_FILENAME']
 app_lifecycle_tag = os.environ['APP_LIFECYCLE_TAG']
 week_to_execute = os.environ['WEEK_TO_EXECUTE']
-all_our_snapshots = []
 s3_bucket = s3_resource.Bucket(bucket_name)
 now = datetime.datetime.now()
 
@@ -38,7 +37,6 @@ def create_rds_instances():
         print "ERROR: Error converting S3 text file to dictionary: %s" % err
 
     for instance in rds_instances_metadata.keys():
-        db_snapshot_identifier = ''
         db_instance_name = instance.lower()
         db_instance_identifier = rds_instances_metadata[instance]['DBInstanceIdentifier'].lower()
         snapshot_id_partial = db_instance_name + '-lifecycle-snapshot-'
@@ -49,33 +47,23 @@ def create_rds_instances():
         except Exception, err:
             print "ERROR: Error retreiving %s snapshots metadata: %s" % (db_instance_identifier, err)
 
-        # Loop through all of this DB instance's snapshots to find one that was
-        # created by RDS.
-        # The delete process should keep the most recent RDS-created snapshot.
+        # Loop through all of this DB instance's snapshots to find the most recent one that was
+        # created using this lifecycle process.
+        snap_date_ids = {}
         for snapshot in snapshots['DBSnapshots']:
             if snapshot_id_partial in snapshot['DBSnapshotIdentifier']:
-                all_our_snapshots.append(snapshot)
+                snap_date_ids[snapshot['SnapshotCreateTime']] = snapshot['DBSnapshotIdentifier']
 
-                db_snapshot_identifier = snapshot['DBSnapshotIdentifier']
-
-        snapshot_dates = []
-        for snapshot in all_our_snapshots:
-            snapshot_dates.append(snapshot['SnapshotCreateTime'])
-
-        print snapshot_dates
+        snapshot_dates = snap_date_ids.keys()
         snapshot_dates.sort()
-        print snapshot_dates[-1]
-        most_recent_snapshot_date = snapshot_dates[-1]
-
-        for snapshot in all_our_snapshots:
-            if snapshot['SnapshotCreateTime'] == most_recent_snapshot_date:
-                db_snapshot_identifier = snapshot['DBSnapshotIdentifier']
-        print "Latest snapshot date: %s, snapshot id: %s" % (most_recent_snapshot_date, db_snapshot_identifier)
+        current_snap_date = snapshot_dates[-1]
+        db_snapshot_identifier = snap_date_ids[current_snap_date]
 
         if not db_snapshot_identifier:
-            print "DB snapshot partial name not found in list of snapshots: %s" % snapshot_id_partial
+            print "NOTICE: No suitable DB snapshot was found in list of snapshots: %s" % snapshot_id_partial
             continue
 
+        # Create a dictionary with our restore params
         restore_kwargs = {
             'DBInstanceIdentifier': rds_instances_metadata[instance]['DBInstanceIdentifier'],
             'DBSnapshotIdentifier': db_snapshot_identifier,
@@ -98,7 +86,7 @@ def create_rds_instances():
         # Create the new instance using the DB snapshot if it exists.
         response = False
         try:
-            #response = rds.restore_db_instance_from_db_snapshot(**restore_kwargs)
+            response = rds.restore_db_instance_from_db_snapshot(**restore_kwargs)
             print "NOTICE: Created DB Instance: %s from DB snapshot: %s" % (db_instance_name, db_snapshot_identifier)
         except Exception, err:
             print "ERROR: Error creating DB Instance %s: %s" % (db_instance_name, err)
